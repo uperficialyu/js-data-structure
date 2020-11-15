eb3b1789ec5db25ba674f820f4ee09017e89e48c
Emily <947201766@qq.com>
2020-11-15 22:08:20 +0800

    冒泡排序2

--- code/sort/sort1.js
+++ code/sort/sort1.js
@@ -2,7 +2,7 @@
  * @Author: yushunping
  * @Date: 2020-11-15 20:37:23
  * @Last Modified by:   yushunping
- * @Last Modified time: 2020-11-15 21:52:38
+ * @Last Modified time: 2020-11-15 22:07:29
 */
 
 const sort1 = (arr) => {
@@ -19,6 +19,20 @@ const sort1 = (arr) => {
   return arr;
 };
 
+const sort2 = (arr) => {
+  for (let i = arr.length - 1, tmp; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      tmp = arr[j]
+      if (tmp > arr[j + 1]) {
+        arr[j] = arr[j + 1]
+        arr[j + 1] = tmp
+      }
+    }
+  }
+  return arr;
+}
+
 export default {
-  sort1
+  sort1,
+  sort2
 };
--- test/sort/sort1.test.js
+++ test/sort/sort1.test.js
@@ -2,12 +2,16 @@
  * @Author: yushunping
  * @Date:   2020-11-15 21:54:43
  * @Last Modified by:   yushunping
- * @Last Modified time: 2020-11-15 21:57:31
+ * @Last Modified time: 2020-11-15 22:07:54
  */
 
 import sort from '../../code/sort/sort1.js';
 
 test('冒泡排序1', () => {
   expect(sort.sort1([1, 9, 5, 3, 4])).toEqual([1, 3, 4, 5, 9])
+});
+
+test('冒泡排序2', () => {
+  expect(sort.sort2([1, 9, 5, 3, 4])).toEqual([1, 3, 4, 5, 9])
 });
 
